e77ecc1c2837d75eb411a090906bf34fd23a59e5
omid <omid.b193@gmail.com>
2026-05-16 18:17:23 +0430

    some confing for better DX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  cacheComponents: !isDev, // در dev = false، در production = true
+  reactCompiler: !isDev, // در dev = false، در production = true
 };
 
 export default nextConfig;
